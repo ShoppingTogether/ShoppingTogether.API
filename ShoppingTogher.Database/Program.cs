36052fbfef57751a2bfb9fca2dcb3f9856fa0714
Omar Ahmed <omarahmed1121@gmail.com>
2023-08-27 15:26:31 -0500

    pull connection string from arguments for db migration

--- ShoppingTogher.Database/Program.cs
+++ ShoppingTogher.Database/Program.cs
@@ -2,8 +2,19 @@
 using DbUp.Postgresql;
 using System.Reflection;
 
-//Pull connection string from AWS Secrets Manager
-var connectionString = "";
+//Pull connection string from secret store and pass in CI/CD
+var connectionString = args.FirstOrDefault(x => x.StartsWith("--ConnectionString", StringComparison.OrdinalIgnoreCase))
+    ?? "";
+
+if (String.IsNullOrEmpty(connectionString))
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Please provide a connection string");
+    Console.ResetColor();
+    return -1;
+}
+
+connectionString = connectionString.Substring(connectionString.IndexOf("=") + 1).Replace(@"""", string.Empty);
 
 EnsureDatabase.For.PostgresqlDatabase(connectionString);
 var upgrader =
